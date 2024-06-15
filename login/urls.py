6cbbff7b6f3cb711305606bf6efb9b5f8f8add39
Vitor Melech Venante <vitor.venante@hotmail.com>
2024-06-14 22:25:39 -0300

    login de autenticacao basico finalizado

--- login/urls.py
+++ login/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import pagina_login
+from .views import pagina_login, pagina_cadastro
 
 urlpatterns = [
-    #     LOGIN
-    path('', pagina_login, name='pagina_login'),
+    # LOGIN
+    path('',            pagina_login,       name='pagina_login'),
+
+    # CADASTRO
+    path('cadastro/',   pagina_cadastro,    name='pagina_cadastro'),
+
 ]
--- login/views.py
+++ login/views.py
@@ -1,5 +1,46 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # LOGIN
 def pagina_login(request):
-    return render(request, 'login/login.html')
+    if request.method == 'GET':
+        return render(request, 'login/login.html')
+    else:
+        # obtendo os dados inseridos nos campos ' usuario ' e ' senha '.
+        usuario    = request.POST.get('usuario')
+        senha       = request.POST.get('senha')
+
+        usuario = authenticate(username=usuario, password=senha)
+
+        if usuario:
+            login(request, usuario)
+
+            return HttpResponse('autenticado.')
+        else:
+            return HttpResponse('Email ou Senha inválidos.')
+
+def pagina_cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'login/cadastro.html')
+    else:
+        # pegando os dados da pág. cadastro
+        usuario = request.POST.get('usuario')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        # acessando o Db e filtrando o usuario, retornando o primeiro objeto
+        usuario_cadastrado = User.objects.filter(username=usuario).first()
+
+        if usuario_cadastrado:
+            return HttpResponse('Já existe um usuário com o mesmo nome.')
+
+        # criando um novo usuario
+        usuario_novo = User.objects.create_user(
+            username=usuario,
+            email   =email,
+            password=senha
+        )
+        usuario_novo.save()
+        return HttpResponse('Usuário cadastrado com sucesso.')
