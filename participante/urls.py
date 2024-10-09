d2caa85326320c595d57b4f00ec649c573455bd9
Vitor Melech Venante <vitor.venante@hotmail.com>
2024-10-09 19:49:36 -0300

    #9 - criacao pag. relatorios e editarUsuario

--- participante/urls.py
+++ participante/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-from .views import participante, salvar_participante, editar_participante, atualizar_participante, deletar_participante, analisar_participante
+from .views import participante, salvar_participante, editar_participante, atualizar_participante, deletar_participante, analisar_participante, relatorios
 
 urlpatterns = [
     # CADASTRO PARTICIPANTES
@@ -11,7 +11,9 @@ urlpatterns = [
     path('editar_participante/<int:id>',    editar_participante,        name='editar_participante'),
     path('atualizar_participante/<int:id>', atualizar_participante,     name='atualizar_participante'),
     path('deletar_participante/<int:id>',   deletar_participante,       name='deletar_participante'),
-    path('analisar_participante/<int:id>',  analisar_participante,      name='analisar_participante')
+    path('analisar_participante/<int:id>',  analisar_participante,      name='analisar_participante'),
+
+    path('relatorios/',                     relatorios,                 name='relatorios')
 ]
 
 if settings.DEBUG:
